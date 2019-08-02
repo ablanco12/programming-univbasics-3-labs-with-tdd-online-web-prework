14076a1aefc36bbb9c41efd6964e5f2114adcc14
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-08-02 07:04:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,9 @@
-# Add your variables here
+first_number("integer","Float")
+second_number("integer","Float")
 get_variable_from_file("integer","Float")
+
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number*second_number
+quotient = first_number/second_number
